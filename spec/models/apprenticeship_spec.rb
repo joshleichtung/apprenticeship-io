3599d104c79d2c836321db4725ad30485ff16dab
Josh Leichtung <joshleichtung@gmail.com>
2017-11-06 19:52:05 -0800

    Add model tests to Apprentice

--- spec/models/apprenticeship_spec.rb
+++ spec/models/apprenticeship_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Apprenticeship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(
+      company_name: "LinkedIn", 
+      city: "San Francisco",
+      state: "CA",
+      url: "example.com"
+  ) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a company name" do
+    subject.company_name = nil
+    expect(subject).not_to be_valid
+    subject.company_name = "LinkedIn"
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a location" do 
+    subject.city = nil
+    subject.state = nil
+    expect(subject).not_to be_valid
+    subject.city = "San Francisco"
+    expect(subject).to be_valid
+    subject.city = nil
+    subject.state = "CA"
+    expect(subject).to be_valid
+    subject.city = "San Francisco"
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a url"do
+    subject.url = nil
+    expect(subject).not_to be_valid
+  end
 end
