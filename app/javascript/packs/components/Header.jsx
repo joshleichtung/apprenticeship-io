bf475a888d681752d02cace7139914a0c0d47830
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-11-09 19:49:33 -0800

    Style the header so the nav links are inline and float right with the logo on the far left.

--- app/javascript/packs/components/Header.jsx
+++ app/javascript/packs/components/Header.jsx
@@ -4,17 +4,41 @@ import { Link } from 'react-router-dom'
 export default class Header extends Component{
   render(){
     return(
-    <div className="header">
-      <div className="logo">
+    <div className="header" style={styles.header}>
+      <div className="logo" style={styles.logo}>
+
       </div>
-      <div className="header-links">
+      <div className="header-links" style={styles.header_links}>
         <ul>
-          <li><a href="/">Employers</a></li>
-          <li><a href="/">Apprentices</a></li>
-          <li><a href="/">Join the Communitty</a></li>
+          <li style={styles.header_links_li}><a href="/">Employers</a></li>
+          <li style={styles.header_links_li}><a href="/">Apprentices</a></li>
+          <li style={styles.header_links_li}><a href="/">Join the Communitty</a></li>
         </ul>
       </div>
     </div>
     )
+  }
+}
+
+const styles = {
+  header: {
+      width: '100%',
+      height: '60px',
+      background: 'white',
+      margin: 'auto'
+  },
+  logo: {
+    width: '15%',
+    height: '100%',
+    float: 'left'
+  },
+  header_links: {
+    float: 'right',
+    position: 'relative',
+    top: '35%'
+  },
+  header_links_li: {
+    display: 'inline',
+    padding: '10px'
   }
 }
