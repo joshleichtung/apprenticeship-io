6abd037d4cfe80a2674ca5db395b9e765fa11f15
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-11-09 22:09:11 -0800

    Center all the text within the div

--- app/javascript/packs/components/Footer.jsx
+++ app/javascript/packs/components/Footer.jsx
@@ -4,63 +4,68 @@ export default class Footer extends Component {
   render(){
     return(
       <footer style={styles.footer}>
+        <div className="links-container" style={styles.links_container}>
+          <div className="links" style={styles.links}>
+          <ul style={styles.ul}>
+            <li>
+              Home
+            </li>
+            <li>
+              Concept
+            </li>
+            <li>
+              Value
+            </li>
+            <li>
+              Resources
+            </li>
+          </ul>
+        </div>
         <div className="links" style={styles.links}>
-        <ul style={styles.ul}>
-          <li>
-            Home
-          </li>
-          <li>
-            Concept
-          </li>
-          <li>
-            Value
-          </li>
-          <li>
-            Resources
-          </li>
-        </ul>
+          <ul style={styles.ul}>
+            <li>
+              Employers
+            </li>
+            <li>
+              Concept & Value
+            </li>
+            <li>
+              Map of Apprenticeships
+            </li>
+            <li>
+              Resources
+            </li>
+          </ul>
+        </div>
+        <div className="links" style={styles.links}>
+          <ul style={styles.ul}>
+            <li>
+              Apprentices
+            </li>
+            <li>
+              Concept & Value
+            </li>
+            <li>
+              Map of Apprenticeships
+            </li>
+            <li>
+              Hall of Fame
+            </li>
+          </ul>
+        </div>
+        <div className="links" style={styles.links}>
+          <ul style={styles.ul}>
+            <li>
+              About Us
+            </li>
+            <li>
+              Contact
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="links" style={styles.links}>
-        <ul style={styles.ul}>
-          <li>
-            Employers
-          </li>
-          <li>
-            Concept & Value
-          </li>
-          <li>
-            Map of Apprenticeships
-          </li>
-          <li>
-            Resources
-          </li>
-        </ul>
-      </div>
-      <div className="links" style={styles.links}>
-        <ul style={styles.ul}>
-          <li>
-            Apprentices
-          </li>
-          <li>
-            Concept & Value
-          </li>
-          <li>
-            Map of Apprenticeships
-          </li>
-          <li>
-            Hall of Fame
-          </li>
-        </ul>
-      </div>
-      <div className="links" style={styles.links}>
-        <ul style={styles.ul}>
-          <li>
-            About Us
-          </li>
-          <li>
-            Contact
-          </li>
-        </ul>
+      <div className="join" style={styles.join}>
+        Join the Community
       </div>
       </footer>
     )
@@ -75,9 +80,18 @@ const styles = {
     color: 'white'
   },
   ul: {
-    listStyle: 'none'
+    listStyle: 'none',
+    paddingRight: '40px'
   },
   links: {
     display: 'table-cell'
+  },
+  links_container: {
+    width: '50%',
+    margin: '0 auto'
+  },
+  join: {
+    textAlign: 'center',
+    margin: '15px'
   }
 }
