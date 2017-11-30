e52f2cae542263ba0294e0b2dc0876efbc7bb542
Josh Leichtung <joshleichtung@gmail.com>
2017-11-30 15:11:15 -0800

    Add profile route and logout rout

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :apprentices, only: [:index]
+    get '/profile', to: 'apprentices#show'
   end
 
   get '/auth/linkedin/callback', to: 'sessions#apprentice_login'
+  get '/auth/linkedin/logout', to: 'sessions#apprentice_logout'
 
   root 'pages#home'
 
